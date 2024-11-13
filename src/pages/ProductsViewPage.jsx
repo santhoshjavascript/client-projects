f94be5386eccacb084d724e8fc9e4951722dea43
kavinesh <kavineshkavinesh860@gmail.com>
2024-11-13 09:23:47 +0530

    productviewpage

--- src/pages/ProductsViewPage.jsx
+++ src/pages/ProductsViewPage.jsx
@@ -1,5 +1,17 @@
+import ProductsView from "../features/ProductView/components/ProductsView";
+import Footer from "../features/Footer/components/Footer";
+import NavBar from "../features/Header/components/NavBar";
+
 function ProductsViewPage() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+
+      <ProductsView />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default ProductsViewPage;
