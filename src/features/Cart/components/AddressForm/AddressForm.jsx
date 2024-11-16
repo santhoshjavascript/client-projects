cb74f9904aa70e8795bd216e21f6ee3930407a83
kavinesh <kavineshkavinesh860@gmail.com>
2024-11-17 00:35:15 +0530

    address form

--- src/features/Cart/components/AddressForm/AddressForm.jsx
+++ src/features/Cart/components/AddressForm/AddressForm.jsx
@@ -1,14 +1,33 @@
 import styles from "./AddressForm.module.css";
-import { Button } from "@chakra-ui/react";
+import LogoName from "../../../../components/Logo";
 
 function AddressForm({ nextStep }) {
   return (
-    <div className={styles.addressFormBox}>
-      <Button colorScheme="blue" size="lg">
-        Click Me
-      </Button>
-    </div>
+    <section className={styles.addressFormBox}>
+      <header className={styles.checkHeader}>
+        <figure>
+          <LogoName />
+        </figure>
+      </header>
+    </section>
   );
 }
 
 export default AddressForm;
+
+//  {
+//    /* <h2>Address Form</h2>
+//       <form>
+//         <label htmlFor="name">Name:</label>
+//         <input type="text" id="name" name="name" required />
+//         <label htmlFor="address">Address:</label>
+//         <input type="text" id="address" name="address" required />
+//         <label htmlFor="city">City:</label>
+//         <input type="text" id="city" name="city" required />
+//         <label htmlFor="state">State:</label>
+//         <input type="text" id="state" name="state" required />
+//         <label htmlFor="zip">Zip:</label>
+//         <input type="text" id="zip" name="zip" required />
+//         <button onClick={nextStep}>Next</button>
+//       </form> */
+//  }
