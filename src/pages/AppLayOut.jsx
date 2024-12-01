83dabdfa57143e35128105dc30baec78d87d0913
kavinesh <kavineshkavinesh860@gmail.com>
2024-12-01 22:16:03 +0530

    responsive navbar

--- src/pages/AppLayOut.jsx
+++ src/pages/AppLayOut.jsx
@@ -1,24 +1,24 @@
-import { Outlet, useLocation, useNavigation } from "react-router-dom";
-import Footer from "../features/Footer/components/Footer";
-import NavBar from "../features/Header/components/NavBar";
+import { Outlet, useNavigation } from "react-router-dom";
+// import Footer from "../features/Footer/components/Footer";
+// import NavBar from "../features/Header/components/NavBar";
 import Spinner from "../components/Spinner/Spinner";
 
 export default function AppLayOut() {
   const navigate = useNavigation();
 
-  const location = useLocation();
+  // const location = useLocation();
 
-  const noHeaderFooterRoutes = ["/cart/address", "/cart/checkout"];
+  // const noHeaderFooterRoutes = ["/cart/address", "/cart/checkout"];
 
-  const hideHeaderFooter = noHeaderFooterRoutes.includes(location.pathname);
+  // const hideHeaderFooter = noHeaderFooterRoutes.includes(location.pathname);
 
   if (navigate.state === "loading") return <Spinner />;
 
   return (
     <div>
-      {!hideHeaderFooter && <NavBar />}
+      {/* {!hideHeaderFooter && <NavBar />} */}
       <Outlet />
-      {!hideHeaderFooter && <Footer />}
+      {/* {!hideHeaderFooter && <Footer />} */}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
 // import NavBar from "../features/Header/components/NavBar";
+import styles from "../features/Header/components/Navbar.module.css";
 import Hero from "../features/Hero/components/Hero";
 import ProductList from "../features/ProductList/components/ProductList";
 import Gender from "../features/Gender/components/Gender";
+import NavBar from "../features/Header/components/NavBar";
+import Footer from "../features/Footer/components/Footer";
+import { useState } from "react";
 
 function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleMenuToggle() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <main>
+    <main className={styles.main}>
+      <div
+        className={`${styles.overlay} ${isMenuOpen ? styles.open : ""}`}
+        onClick={handleMenuToggle}
+      ></div>
+      <NavBar isMenuOpen={isMenuOpen} handleMenuToggle={handleMenuToggle} />
       <Hero />
       <ProductList type={"Hot Collections"} />
       <ProductList type={"Featured Products"} />
       <Gender />
       <ProductList type={"Featured Products"} />
+      <Footer />
     </main>
   );
 }
